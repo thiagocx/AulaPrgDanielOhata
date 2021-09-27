05e8f752070ba536aa6993cb76c7a2943bc8771d
Thiago marcos <thiagomarcos2015@hotmail.com>
2021-09-26 21:45:41 -0300

    lição ohata dadospessoas

---
+++ dadosPessoas/Dadospessoa/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dadospessoa
+{
+    class pessoa
+    {
+        public string nome;
+        public int idade;
+    }
+
+}
