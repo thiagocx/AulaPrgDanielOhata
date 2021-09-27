c212847ec1ed159fb0a3aa5ce854f16ba12e1766
Thiago marcos <thiagomarcos2015@hotmail.com>
2021-09-26 21:44:49 -0300

    Lição ohata_areatriangulo

---
+++ areatriângulo/areatriângulo/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ladotriângulo
+{
+	class trianguloX
+	{
+		public double A;
+		public double B; 
+		public double C;	
+		public double D;
+	}
+
+    class trianguloY
+    {
+		public double A;
+		public double B;
+		public double C;
+		public double D;
+	}
+}
