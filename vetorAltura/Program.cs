a82ca7d9ef6771b4c950a37d91e2953593be4afc
Thiago marcos <thiagomarcos2015@hotmail.com>
2021-10-24 14:11:11 -0300

    Exercício individual - Tema: Array/Classe

---
+++ vetorAltura/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace vetorAltura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("n° de pessoas: ");
+            //Criando a variável de entrada
+            int n = int.Parse(Console.ReadLine());
+            //Criando vetor com o número da variável de entrada
+            double[] vect = new double[n];
+            //Utilização do comando for para percorrer o vetor
+            for (int i = 0; i < n; i++)
+            {
+                //Entrada dos elementos no vetor pelo usuário
+                Console.WriteLine("altura da pessoa " + i +":");
+                vect[i] = double.Parse(Console.ReadLine(),
+                CultureInfo.InvariantCulture);
+            }
+            //Variável soma
+            double sum = 0.0;
+            //Utilização do for para percorrer o vetor
+            for (int i = 0; i < n; i++)
+            {
+                //Somatoria dos elementos inseridos
+                sum += vect[i];
+            }
+            double media = sum / n;
+            Console.WriteLine("O valor da média das alturas informadas é: " +  media);
+        }
+    }
+}
